946ba611508ddc7ccfb19fb5ff34e5927cd1a83a
hjoon-code <195007133+hjoon-code@users.noreply.github.com>
2025-04-07 21:06:55 -0400

    Create ChartBuilder.java

---
+++ src/ChartBuilder.java
@@ -0,0 +1,12 @@
+package stockpricesorter;
+
+public class ChartBuilder {
+    // TODO: Add method to generate line chart for performance comparison
+
+    // Example:
+    // public static void createLineChart(String title, String xLabel, String yLabel, Map<String, List<Integer>> data) {
+    //     // Use JavaFX or JFreeChart to draw chart
+    // }
+
+    // Beth can decide on the library and layout
+}
