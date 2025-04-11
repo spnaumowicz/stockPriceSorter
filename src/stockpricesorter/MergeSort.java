7b1b7436793eaa1e218af5891b92db60a8607e4a
hjoon-code <195007133+hjoon-code@users.noreply.github.com>
2025-04-11 14:55:09 -0400

    Update MergeSort.java
    import stockpricesorter.StockPriceSorter.SortResult;

--- src/stockpricesorter/MergeSort.java
+++ src/stockpricesorter/MergeSort.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import stockpricesorter.StockData;
+import stockpricesorter.StockPriceSorter.SortResult;
 
 public class MergeSort {
 
